0cb8f621ef0381661ea24c1ee978a6e291a6da80
thomas <thomasmonta98@gmail.com>
2021-07-18 16:57:11 +1000

    done with assignment

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,6 +10,7 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+    drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
   
   /* TODO: Create an event handler which, when fired, invokes the provided
@@ -28,6 +29,8 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={toggleCycling}
+        onKeyPress={e => {if(e.key === 'a'){return resize('+')}else if(e.key === 's'){return resize('-')}}}
         width='900'
         height='600'
         tabIndex="0">
